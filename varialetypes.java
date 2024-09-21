cc10635cb4680d21032aeb7f7ff0a28226f40a93
Balasaravanan s <balasaravanan062@gmail.com>
2024-09-21 12:23:47 +0530

    added new file

---
+++ varialetypes.java
@@ -0,0 +1,16 @@
+public class VariableTypesDemo {
+    public static void main(String[] args) {
+        
+        int integerVar = 10;          // Integer type variable
+        double doubleVar = 3.14;     // Double type variable
+        char charVar = 'A';          // Character type variable
+        boolean boolVar = true;      // Boolean type variable
+        String stringVar = "Hello";   // String type variable (corrected quotation mark)
+        
+        System.out.println("Integer variable: " + integerVar);
+        System.out.println("Double variable: " + doubleVar);
+        System.out.println("Character variable: " + charVar);
+        System.out.println("Boolean variable: " + boolVar);
+        System.out.println("String variable: " + stringVar);
+    }
+}
