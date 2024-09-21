55d78b9c96e021ca39d652349464ede309b5b6fd
Balasaravanan s <balasaravanan062@gmail.com>
2024-09-21 12:41:22 +0530

    added new file

---
+++ oddeven.java
@@ -0,0 +1,15 @@
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        if (isEven(number)) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+}
